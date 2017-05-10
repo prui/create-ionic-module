fb8c2337bf8c76dc38773dbfe6a85bec1427a73a
pengruiqing <pengruiqing@szxhinfo.com>
2017-05-10 17:09:20 +0800

    Update

--- src/generate.js
+++ src/generate.js
@@ -43,12 +43,12 @@
     }
 
     function writeFile(name, suffix, tag, type) {
-        // if (tag == 'Router' && type == 'ionSideMenus') {
-        //     var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'ISMBase' + suffix));
-        // } else {
-        //     var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'Base' + suffix));
-        // }
-        var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'Base' + suffix));
+        if (tag == 'Router' && type == 'ionSideMenus') {
+            var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'ISMBase' + suffix));
+        } else {
+            var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'Base' + suffix));
+        }
+        // var temp = fs.readFileSync(path.resolve(__dirname, '..', 'template', 'Base' + suffix));
         var newFileText = temp.toString().replace(/Base/g, name);
 
         if (tag == 'Router') {
